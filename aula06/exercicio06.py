a28c714d9a8c6e4bd53ed63a66a302cdeaacefec
Leandro Cardoso <leandrocardosodev@gmail.com>
2024-04-23 12:11:36 -0300

    Aula 06 exercicios 06 e 07

--- aula06/exercicio06.py
+++ aula06/exercicio06.py
@@ -6,4 +6,16 @@ i, j = 1
 Somatório de um somatório (i + j)
 '''
 
+def somatorio(n:int) -> int:
+    soma = 0
+    for i in range(1, n + 1):
+        soma_interna = 0
+        for j in range(1, n + 1):
+            soma_interna += i + j
+        soma += soma_interna
+    return soma
 
+n = 3
+soma = somatorio(n)
+
+print(f'\nSOMATORIO: {soma}\n')
--- aula06/exercicio07.py
+++ aula06/exercicio07.py
@@ -6,4 +6,15 @@ i, j = 1
 Somatório de um somatório (1 / i, j) ** 2
 '''
 
+def somatorio(n:int) -> list:
+    somas = [0, 0]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            somas[0] += (1 / i) ** 2
+            somas[1] += j ** 2
+    return somas
 
+n = 3
+somas = somatorio(n)
+
+print(f'\nSOMATORIO: {somas[0]:.2f} e {somas[1]}\n')
